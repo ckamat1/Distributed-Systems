4a0242e7eda1ade8f4ddc90ea3a6a69415f6a3f6
Chirag Kamat <chka1976@colorado.edu>
2017-04-17 17:57:38 -0700

    The application will now count the number of google books per n-gram

--- PA3/Spark/App1.py
+++ PA3/Spark/App1.py
@@ -2,6 +2,7 @@ from pyspark import SparkContext, SparkConf
 import os
 import sys
 from nltk.tokenize import RegexpTokenizer
+from operator import add
 
 
 tokenizer = RegexpTokenizer(r'\w+')
@@ -13,18 +14,29 @@ def tokenize(text):
 def tokens(text):
     return [line.split() for line in text]
 
+def getListofTuples(text):
+    return [(l.split()[0],l.split()[3]) for l in open(text) if l is not None]
 
 
-path = "/home/ckamat/PA2_Raft/Distributed-Systems/PA3/Spark/"
-logFile = "shakespeare.txt"  # Should be some file on your system
-logFile = path + logFile
+
+# path = "/home/ckamat/PA2_Raft/Distributed-Systems/PA3/Spark/"
+path = "/root/source/Distributed-Systems/PA3/Spark/"
+# logFile = "n-grams.txt"  # Should be some file on your system
+# logFile = path + logFile
+logFile = "s3://datasets.elasticmapreduce/ngrams/books/20090715/eng-us-all/1gram/data"
 conf = SparkConf().setAppName("Simple App").setMaster("local")
 sc = SparkContext(conf=conf)
 lines = sc.textFile(logFile)
-tokensRDD = lines.flatMap(lambda line: line.split(" "))
-pairs = tokensRDD.map(lambda s: (s, 1))
-counts = pairs.reduceByKey(lambda a, b: a + b)
-counts.saveAsTextFile(path+"output")
+tuplesRDD = lines.map(lambda line: (line.split()[0],int((line.split()[4]))))
+# partitionRDD = sc.parallelize(tuplesRDD.collect())
+booksRDD = tuplesRDD.reduceByKey(add)
+booksRDD.saveAsTextFile(path+'output2')
+counts = booksRDD.collect()
+
+#
+# pairs = tokensRDD.map(lambda s: (s, 1))
+# counts = pairs.reduceByKey(lambda a, b: a + b)
+# counts.saveAsTextFile(path+"output")
 print counts
 
 
